7e06dd1c6d95f0bc284c30ce8a09225cd5f698c9
3dw1nM0535 <workockmoses@gmail.com>
2020-06-21 22:52:16 +0300

    ch(redux integration)

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,35 @@
+// Redux
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Redux thunk(api request using redux)
 import thunk from 'redux-thunk';
 
+// Redux persist
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage
+
+// Root reducer
 import { rootReducer } from '../reducers';
 
-export const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
+
+// Config persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+// Persist reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+// Configure application data store
+const store = createStore(
+  persistedReducer,
+  composeWithDevTools(applyMiddleware(thunk))
 );
+
+const persistor = persistStore(store);
+
+export { store, persistor };
