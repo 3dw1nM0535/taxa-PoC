b93e93af11dc31643f64e3e1e4ba25d5b9bfd27b
3dw1nM0535 <workockmoses@gmail.com>
2020-07-02 21:43:57 +0300

    install pkgs

--- src/store/index.js
+++ src/store/index.js
@@ -5,31 +5,14 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 // Redux thunk(api request using redux)
 import thunk from 'redux-thunk';
 
-// Redux persist
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage
-
 // Root reducer
 import { rootReducer } from '../reducers';
 
 
-// Config persist
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['user'],
-};
-
-// Persist reducer
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-
 // Configure application data store
 const store = createStore(
-  persistedReducer,
+  rootReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
 
-const persistor = persistStore(store);
-
-export { store, persistor };
+export { store };
