e0541526505f890e2fe1b17b124e681df7ad91b4
3dw1nM0535 <workockmoses@gmail.com>
2020-07-21 18:31:23 +0300

    chore(style component)

--- src/components/farm/FarmCard.js
+++ src/components/farm/FarmCard.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { ClickableTile } from 'carbon-components-react'
 
-export function FarmCard() {
+export function FarmCard({ farm }) {
   return (
     <ClickableTile
+      className="spacing"
       href="#"
       light={false}
     >
       <div className="img--container">
         <img src="https://swarm-gateways.net/bzz:/e2f2f74a6d15829b7cda29d46a5ee6b308382e66ab02856202e2f6c2ade35da7/african_farmer.jpg" alt="farm" width="200" height="200" />
+      </div>
+      <div className="caption">
+        <p># {farm.id}</p>
+        <p>{farm.size}</p>
+      </div>
+      <hr />
+      <div className="caption">
+        <p>{farm.soil}</p>
+        <p>location</p>
       </div>
     </ClickableTile>
   )
--- src/components/pages/Farms.js
+++ src/components/pages/Farms.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import { Grid, Row, Column } from 'carbon-components-react'
+import { gql, useQuery } from '@apollo/client'
 
 import { FarmCard } from '../farm'
 
 export function Farms() {
+
+  const GET_FARMS = gql`
+    query GetFarms {
+      getFarms {
+        id
+        size
+        soil
+        imageHash
+      }
+    }
+  `
+  const { loading, data, error } = useQuery(GET_FARMS)
+
+  if (loading) return (<p>Loading...</p>)
+  if (error) return (<p>Error: {error.message}</p>)
+
   return (
     <Grid>
       <Row>
-        <Column className="farm--card" lg={2}>
-          <FarmCard />
-          <div className="caption">
-            <p className="token--caption"># token</p>
-            <p className="size--caption">size</p>
-          </div>
-          <div>
-            <p>location</p>
-          </div>
-        </Column>
+        {data.getFarms.length > 0 ? data.getFarms.map(farm => (
+          <Column key={farm.id} className="farm--card" lg={16}>
+            <FarmCard farm={farm} />
+          </Column>
+        )) : (
+          <Column className="info--section">
+            <h1>No farms to be found</h1>
+          </Column>
+        )}
       </Row>
     </Grid>
   )
