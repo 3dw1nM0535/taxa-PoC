389bd736ae0ccf288881289aa4cbffdad8423064
3dw1nM0535 <workockmoses@gmail.com>
2020-08-05 00:31:56 +0300

    chore(UI refactor)

--- src/components/farm/FarmComponent.js
+++ src/components/farm/FarmComponent.js
@@ -11,13 +11,15 @@ import {
   Header,
   Popup,
   Icon,
+  Table,
 } from 'semantic-ui-react'
+import { truncateAddress } from '../../utils'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import GoogleMapReact from 'google-map-react'
 import { useParams } from 'react-router-dom'
 import { LocationPin } from './Location'
 
-export function FarmComponent({ farmData }) {
+export function FarmComponent({ farmData, wallet }) {
   
   const { tokenId } = useParams()
   const [copying, setCopying] = useState(true)
@@ -35,16 +37,18 @@ export function FarmComponent({ farmData }) {
                 horizontal
               />
             </Segment>
-            <Segment.Group>
+            <Segment>
               <Image src={farmData.imageHash === undefined ?
                 'https://react.semantic-ui.com/images/wireframe/square-image.png' : `https://ipfs.io/ipfs/${farmData.imageHash}`} fluid />
-            </Segment.Group>
+            </Segment>
             <Segment>
-              <Button
-                color={farmData.season === 'Dormant' ? 'brown' : farmData.season === 'Harvest' ? 'green' : null}
-              >
-                Change Season
-              </Button>
+              { wallet.address === undefined ? null : wallet.address === farmData.owner ? (
+                  <Button
+                    color={farmData.season === 'Dormant' ? 'brown' : farmData.season === 'Harvest' ? 'green' : null}
+                  >
+                    Change Season
+                  </Button>
+                ) : null } 
             </Segment>
           </Segment.Group>
         </Grid.Column>
@@ -54,79 +58,102 @@ export function FarmComponent({ farmData }) {
               <Header as='h1'>Owner</Header>
             </Segment>
             <Segment>
-            <Segment.Inline>
-              <Image src={makeBlockie(String(farmData.owner))} size="tiny" circular avatar />
-              <Popup
-                trigger={
-                  <Label
-                    as='a'
-                    target='blank'
-                    color='grey'
-                    href={`https://etherscan.io/address/${farmData.owner}`}
-                  >
-                    {farmData.owner}
-                  </Label>
-                }
-                content='View address on etherscan'
-                inverted
-              />
-              <span>
-                <CopyToClipboard
-                  text={farmData.owner}
-                  onCopy={() => setTimeout(() => {
-                    setCopying(false)
-                    setCopied(true)
+              <Table definition>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell width={2}>
+                      Owner
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Image style={{ width: 30, height: 30 }} src={makeBlockie(String(farmData.owner))} size="tiny" circular avatar />
+                      <Popup
+                        trigger={
+                          <Label
+                            as='a'
+                            target='blank'
+                            color='grey'
+                            href={`https://etherscan.io/address/${farmData.owner}`}
+                          >
+                            {farmData.owner === undefined ? null : truncateAddress(farmData.owner, 25)}
+                          </Label>
+                        }
+                        content='View address on etherscan'
+                        inverted
+                      />
+                      <span>
+                        <CopyToClipboard
+                          text={farmData.owner}
+                          onCopy={() => setTimeout(() => {
+                              setCopying(false)
+                              setCopied(true)
 
-                    setTimeout(() => {
-                      setCopying(true)
-                      setCopied(false)
-                    }, 3000)
-                  }, 500)}
-                >
-                  <span>
-                    {copied &&
-                        <Label
-                          as={Button}
-                          horizontal color='green'
-                          size='medium'
+                              setTimeout(() => {
+                              setCopying(true)
+                              setCopied(false)
+                            }, 3000)
+                          }, 500)}
                         >
-                          Copied
-                        </Label>}
-                    {copying &&
-                        <Label
-                          as={Button}
-                          horizontal
-                          size='medium'
-                        >
-                          Copy
-                        </Label>}
-                  </span>
-                </CopyToClipboard>
-              </span>
-            </Segment.Inline>
+                          <span>
+                            {copied &&
+                              <Label
+                                as={Button}
+                                horizontal color='green'
+                                size='medium'
+                              >
+                                Copied
+                              </Label>}
+                            {copying &&
+                              <Label
+                                as={Button}
+                                horizontal
+                                size='medium'
+                              >
+                                Copy
+                              </Label>}
+                          </span>
+                        </CopyToClipboard>
+                      </span>
+                    </Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Size</Table.Cell>
+                    <Table.Cell>{farmData.size}</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Soil</Table.Cell>
+                    <Table.Cell>{farmData.soil}</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Location</Table.Cell>
+                    <Table.Cell>{farmData.location !== undefined ? farmData.location.formatted_address : <span style={{ fontStyle: 'italic' }}>null</span>}</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>#tokenId</Table.Cell>
+                    <Table.Cell>{tokenId}</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>State</Table.Cell>
+                    <Table.Cell>
+                      <Label
+                        content={farmData.season}
+                        color={farmData.season === 'Dormant' ? 'brown' : farmData.season === 'Harvest' ? 'green' : null}
+                        horizontal
+                      />
+                    </Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
             </Segment>
           </Segment.Group>
-          <Segment.Group>
-            <Segment secondary>
-              <Header as='h1'>Size</Header>
-            </Segment>
-            <Segment>
-              <p>{farmData.size}</p>
-            </Segment>
-          </Segment.Group>
-          <Segment.Group>
-            <Segment secondary>
-              <Header as='h1'>Soil</Header>
-            </Segment>
-            <Segment>
-              <p>{farmData.soil}</p>
-            </Segment>
-          </Segment.Group>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column width={16}>
           <Segment.Group>
             <Segment secondary>
               <Header as='h1'>Location</Header>
             </Segment>
-            <Segment style={{ height: '50vh', width: '100%' }}>
+            <Segment style={{ height: '70vh', width: '100%' }}>
               {farmData.location !== undefined ? (
               <GoogleMapReact
                 bootstrapURLKeys={{ key: `${process.env.REACT_APP_GEOCODE_KEY}` }}
@@ -153,14 +180,6 @@ export function FarmComponent({ farmData }) {
               }, 3000))} 
             </Segment>
           </Segment.Group>
-          <Segment.Group>
-            <Segment secondary>
-              <Header as='h1'># Token ID</Header>
-            </Segment>
-            <Segment>
-              <p>{tokenId}</p>
-            </Segment>
-          </Segment.Group>
         </Grid.Column>
       </Grid.Row>
     </Grid>
@@ -169,11 +188,13 @@ export function FarmComponent({ farmData }) {
 
 FarmComponent.propTypes = {
   farmData: PropTypes.object.isRequired,
+  wallet: PropTypes.object.isRequired,
 }
 
 function mapStateToProps(state) {
   return {
     farmData: state.farm,
+    wallet: state.wallet,
   }
 }
 
