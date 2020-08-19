71962b43dc8b06dfad22a845473c819de8260c53
3dw1nM0535 <workockmoses@gmail.com>
2020-08-19 18:26:35 +0300

    feat:fetch bookings data

--- src/components/farm/Booking.js
+++ src/components/farm/Booking.js
@@ -1,11 +1,125 @@
+import PropTypes from 'prop-types'
+import Web3 from 'web3'
 import React from 'react'
 import {
+  Button,
+  Segment,
+  Icon,
+  Table,
   Header,
 } from 'semantic-ui-react'
+import { gql, useQuery } from '@apollo/client'
 
-export function Booking() {
+import { LoaderComponent } from '../loading'
+import { ErrorComponent } from '../error'
+import { connect } from 'react-redux'
+
+function Booking({ farm, conversionRate, wallet }) {
+
+  const GET_BOOKINGS = gql`
+    query GetBookings(
+      $token: Int!
+      $bookerAddress: String!
+    ) {
+      getBookings(input: {
+        token: $token
+        bookerAddress: $bookerAddress
+      }) {
+        id
+        volume
+        booker
+        deposit
+        token
+        delivered
+      }
+    }
+  `
+
+  const { loading, data, error } = useQuery(GET_BOOKINGS, {
+    variables: {
+      token: `${farm.token !== undefined ? +farm.token : 0}`,
+      bookerAddress: `${String(wallet.address[0]).toLowerCase()}`,
+    }
+  })
+  
+  if (loading) return (
+    <>
+      <LoaderComponent loading />
+    </>
+  )
+
+  if (error) return (
+    <>
+      <ErrorComponent error={error} />
+    </>
+  )
+
   return (
-    <Header as='h1'>Booking content</Header>
+    <>
+      {data.getBookings.length === 0 ? (
+        <Segment placeholder>
+          <Header icon>
+            <Icon name='frown' size='large' />
+            No booking data found for this farm
+          </Header>
+        </Segment>
+      ) : (
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Booker</Table.HeaderCell>
+              <Table.HeaderCell>Volume</Table.HeaderCell>
+              <Table.HeaderCell>Deposit</Table.HeaderCell>
+              <Table.HeaderCell>Delivered</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {data.getBookings.map(booking => (
+              <Table.Row key={booking.id}>
+                <Table.Cell>{booking.booker}</Table.Cell>
+                <Table.Cell>{booking.volume}</Table.Cell>
+                <Table.Cell>
+                  {`${Web3.utils.fromWei(booking.deposit)} ETH / KES ${new Intl.NumberFormat('en-US').format(parseInt(parseFloat(Web3.utils.fromWei(booking.deposit)) * parseFloat(conversionRate.ethkes)), 10)}`}
+                </Table.Cell>
+                <Table.Cell>
+                  <Button
+                    size='mini'
+                    color='violet'
+                    disabled={booking.volume === 0}
+                  >
+                    Confirm Received
+                  </Button>
+                  <Button
+                    size='mini'
+                    color='violet'
+                    disabled={booking.volume === 0}
+                  >
+                   Cancel 
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      )}
+    </>
   )
 }
+
+Booking.propTypes = {
+  farm: PropTypes.object.isRequired,
+  conversionRate: PropTypes.object.isRequired,
+  wallet: PropTypes.object.isRequired,
+}
+
+function mapStateToProps(state) {
+  return {
+    farm: state.farm,
+    conversionRate: state.prices,
+    wallet: state.wallet,
+  }
+}
+
+export default connect(mapStateToProps)(Booking)
 
--- src/components/farm/index.js
+++ src/components/farm/index.js
@@ -2,7 +2,7 @@ import { FarmCard } from './FarmCard'
 import { NoFarm } from './NoFarm'
 import FarmComponent from './FarmComponent'
 import Harvest from './Harvest'
-import { Booking } from './Booking'
+import Booking from './Booking'
 import FarmHeader from './FarmHeader'
 
 export {
